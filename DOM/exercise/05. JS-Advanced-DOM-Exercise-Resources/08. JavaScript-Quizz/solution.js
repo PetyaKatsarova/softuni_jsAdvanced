45044229b09568991187a8261a393e0cb9dd9eb3
PetyaKatsarova <pskpetya@yahoo.com>
2021-01-28 15:00:54 +0100

    dom exercises solutions

--- DOM/exercise/05. JS-Advanced-DOM-Exercise-Resources/08. JavaScript-Quizz/solution.js
+++ DOM/exercise/05. JS-Advanced-DOM-Exercise-Resources/08. JavaScript-Quizz/solution.js
@@ -1,13 +1,42 @@
 function solve() {
-    let quiz_answer = document.querySelectorAll(".quiz-answer")
     const section = document.querySelectorAll("section")
+    let correctAnswers = []
+    
+    for(const s of section){
+      s.addEventListener('click', (e)=>{
+          if(e.target.className = 'quiz-answer'){
+              // push correctly clicked answers in correctAnswers arr
+              let p = e.target
+              if(p.textContent == 'onclick' || p.textContent == 'JSON.stringify()' || p.textContent == 'A programming API for HTML and XML documents'){
+                  //console.log(p.textContent)
+                  correctAnswers.push(p.textContent)
+              }
 
-    console.log(quiz_answer)
-    for(let answer of quiz_answer){
-      answer.addEventListener('click', ()=>{
-          if(answer.nextElementSibling && answer.nextElementSibling.classList.contains('hidden')){
-            answer.nextElementSibling.classList.remove('hidden')
+              // hide prev q and display next q
+              if(e.target.parentNode.parentNode.parentNode.parentNode.nextElementSibling && e.target.parentNode.parentNode.parentNode.parentNode.nextElementSibling.classList.contains('hidden')) {
+                  e.target.parentNode.parentNode.parentNode.parentNode.nextElementSibling.classList.remove('hidden')
+                  e.target.parentNode.parentNode.parentNode.parentNode.nextElementSibling.style.display = 'block'
+                  e.target.parentNode.parentNode.parentNode.parentNode.style.display = 'none'
+              }else{
+                  e.target.parentNode.parentNode.parentNode.parentNode.style.display = 'none'
+                  let results = document.getElementById('results')
+                  let h1 = results.getElementsByTagName('h1')[0]
+                  results.style.display = 'block'
+                  h1.textContent = 'kulu'
+
+                  if(correctAnswers.length == 3){
+                      h1.textContent = "You are recognized as top JavaScript fan!"
+                  }else if(correctAnswers.length == 2){
+                      h1.textContent = "You have 2 right answers"
+                  }else if(correctAnswers.length == 1){
+                     h1.textContent = "You have 1 right answers"
+                  }else {
+                    h1.textContent = "You have 0 right answers"
+                  }
+                  
+              }
           }
       })
     }
+   
 }
--- DOM/exercise/05. JS-Advanced-DOM-Exercise-Resources/09. Shopping-Cart/solution.js
+++ DOM/exercise/05. JS-Advanced-DOM-Exercise-Resources/09. Shopping-Cart/solution.js
@@ -1,3 +1,15 @@
 function solve() {
-   //TODO...
+   let addProduct = document.querySelectorAll('.add-product')
+   const textArea = document.querySelector('textarea')
+   const shoppingCart = document.querySelector('.shopping-cart')
+
+   shoppingCart.addEventListener('click', (e)=>{
+      if(e.target.classList.contains('add-product')){
+         let names = document.querySelectorAll('.product-title')
+         let prices = document.querySelectorAll('.product-line-price')
+         for(let i=0; i<names.length;i++){
+            textArea.value = `Added ${names[i].textContent} for ${prices[i].textContent} to the cart.`
+         }
+      }
+   })
 }
